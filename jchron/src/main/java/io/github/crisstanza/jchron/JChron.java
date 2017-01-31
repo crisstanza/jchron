08ec571067bb193c4ab6cbdf4c2ce5b73a6f6380
Cris Stanza <crisstanza@work>
2017-01-31 12:04:09 -0200

    Vs. 1.2 - resultado em segundos
    E facilidade  para concatenar chamadas.

--- jchron/src/main/java/io/github/crisstanza/jchron/JChron.java
+++ jchron/src/main/java/io/github/crisstanza/jchron/JChron.java
@@ -12,6 +12,7 @@ import javax.swing.text.NumberFormatter;
 /**
  * @author Cris Stanza, 17-Ago-2015
  */
+
 public final class JChron {
 
 	private static final float _1000_0_x_60_x_60 = 3600000; // 1000.0 * 60 * 60;
@@ -22,14 +23,13 @@ public final class JChron {
 	private static final int ONE = 1;
 
 	private static final String FORMAT_DEC_3 = "#,##0.###";
-	private static final Locale DEFAULT_LOCALE = new Locale("en", "US");
-	private static final DecimalFormatSymbols DEFAULT_DECIMAL_FORMAT_SYMBOLS = new DecimalFormatSymbols(DEFAULT_LOCALE);
+	private static final DecimalFormatSymbols DEFAULT_DECIMAL_FORMAT_SYMBOLS = new DecimalFormatSymbols(new Locale("en", "US"));
 
 	private static final String READ_FORMAT_HMS = "[ %s hour%s %s min%s %s sec%s ]";
 	private static final String READ_FORMAT_MS = "[ %s min%s %s sec%s ]";
 	private static final String READ_FORMAT_S = "[ %s sec%s ]";
 
-	private static final String ERROR_JCRON_IS_ALREADY_STARTED = "JCron is already started!";
+	private static final String ERROR_JCHRON_IS_ALREADY_STARTED = "JChron is already started!";
 
 	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -38,23 +38,32 @@ public final class JChron {
 	private int estatus = STOP;
 	private Date start = new Date(), stop = start;
 
-	public final void start() {
+	public final JChron start() {
 		if (estatus == START) {
-			throw new IllegalStateException(ERROR_JCRON_IS_ALREADY_STARTED);
+			throw new IllegalStateException(ERROR_JCHRON_IS_ALREADY_STARTED);
 		} else {
 			start = new Date();
 			estatus = START;
 		}
+		return this;
 	}
 
-	public final void stop() {
+	public final JChron stop() {
 		stop = new Date();
 		estatus = STOP;
+		return this;
+	}
+
+	public final String readInSeconds() {
+		return JChron.dec3(this.remainder() / JChron._1000_0);
 	}
 
 	public final String read() {
-		final long remainder = (estatus == STOP ? stop : new Date()).getTime() - start.getTime();
-		return JChron.remainder(remainder);
+		return JChron.remainder(remainder());
+	}
+
+	private long remainder() {
+		return (this.estatus == JChron.STOP ? this.stop : new Date()).getTime() - this.start.getTime();
 	}
 
 	public static final String remainder(long remainder) {
@@ -63,16 +72,12 @@ public final class JChron {
 		final long mins = (long) (remainder / _1000_0_x_60);
 		remainder %= _1000_0_x_60;
 		final float secs = remainder / _1000_0;
-		try {
-			if (hours > 0) {
-				return format(READ_FORMAT_HMS, hours, plural(hours), mins, plural(mins), dec3(secs), plural(secs));
-			} else if (mins > 0) {
-				return format(READ_FORMAT_MS, mins, plural(mins), dec3(secs), plural(secs));
-			} else {
-				return format(READ_FORMAT_S, dec3(secs), plural(secs));
-			}
-		} catch (final Exception exc) {
-			return exc.toString();
+		if (hours > 0) {
+			return format(READ_FORMAT_HMS, hours, plural(hours), mins, plural(mins), dec3(secs), plural(secs));
+		} else if (mins > 0) {
+			return format(READ_FORMAT_MS, mins, plural(mins), dec3(secs), plural(secs));
+		} else {
+			return format(READ_FORMAT_S, dec3(secs), plural(secs));
 		}
 	}
 
@@ -80,8 +85,12 @@ public final class JChron {
 		return n == ONE ? EMPTY : S;
 	}
 
-	private static final String dec3(final float n) throws Exception {
-		return new NumberFormatter(new DecimalFormat(FORMAT_DEC_3, DEFAULT_DECIMAL_FORMAT_SYMBOLS)).valueToString(n);
+	private static final String dec3(final float n) {
+		try {
+			return new NumberFormatter(new DecimalFormat(FORMAT_DEC_3, DEFAULT_DECIMAL_FORMAT_SYMBOLS)).valueToString(n);
+		} catch (final Exception exc) {
+			return exc.toString();
+		}
 	}
 
 }
